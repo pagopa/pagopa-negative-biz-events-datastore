8cea05808774e42eea1f5018a8a4b782fb97926f
AngeloCaporaso <angelo.caporaso@pagopa.it>
2023-05-08 14:34:22 +0200

    [PAGOPA-786] test: Add GHA pipelines

--- integration-test/run_integration_test.sh
+++ integration-test/run_integration_test.sh
@@ -1,26 +1,9 @@
-# example: sh ./run_integration_test.sh <local|dev|uat|prod>
+#!/bin/bash
 
-# create containers
-cd ../docker || exit
-sh ./run_docker.sh "$1"
-
-# waiting the containers
-printf 'Waiting for the service'
-attempt_counter=0
-max_attempts=50
-until $(curl --output /dev/null --silent --head --fail http://localhost:8080/info); do
-    if [ ${attempt_counter} -eq ${max_attempts} ];then
-      echo "Max attempts reached"
-      exit 1
-    fi
-
-    printf '.'
-    attempt_counter=$((attempt_counter+1))
-    sleep 6
-done
-echo 'Service Started'
+# example: sh ./run_integration_test.sh <local|dev|uat>
+set -e
 
 # run integration tests
-cd ../integration-test/src || exit
+cd ./src || exit
 yarn install
-yarn test
+yarn test:"$1"

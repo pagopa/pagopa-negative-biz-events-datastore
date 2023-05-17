6ad57963360f54b7a410787252caec1166d13641
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2023-05-17 18:06:12 +0200

    PAGOPA-911 list int test in progress

--- integration-test/src/step_definitions/support/biz_event_step.js
+++ integration-test/src/step_definitions/support/biz_event_step.js
@@ -69,6 +69,22 @@ Given('a random {string} biz event with id {string}', async function (type, id) 
       assert.strictEqual(responseToCheck.status, 201);
 });
 
+Given('{int} random awakable and {int} final biz events', async function (numAwakable, numFinal) {
+      let totalMessages = new Array();
+      var streamFinal = (createKafkaStream(process.env.EVENT_HUB_NAME_FINAL, process.env.EVENT_HUB_FINAL_RX_CONNECTION_STRING));
+      var streamAwakable = (createKafkaStream(process.env.EVENT_HUB_NAME_AWAKABLE, process.env.EVENT_HUB_AWAKABLE_RX_CONNECTION_STRING));
+      streamFinal.consumer.on('data', (message) => {totalMessages.push(JSON.parse(message.value.toString()))});
+      streamAwakable.consumer.on('data', (message) => {totalMessages.push(JSON.parse(message.value.toString()))});
+      await sleep(10000);
+
+      for (let i = 0; i < numAwakable + numFinal; i++) {
+        await deleteDocument(String(i));
+        let responseToCheck =  await createDocument(String(i), isAwakable);
+        assert.strictEqual(responseToCheck.status, 201);
+      }
+
+});
+
 // When
 When('biz event has been properly stored into datastore after {int} ms', async function (time) {
     // boundary time spent by azure function to process event
@@ -84,4 +100,19 @@ Then('the datastore returns the event with id {string}', async function (targetI
 Then('the eventhub retrieves the event with id {string}', async function (targetId) {
     stream.destroy();
     assert.strictEqual(parsedMessage.id, targetId);
+});
+
+Then('the eventhub retrieves the {int} awakable and {int} final events', async function (numAwakable, numFinal) {
+  streamFinal.destroy();
+  streamAwakable.destroy();
+  let counterAwakable = 0, counterFinal = 0;
+  for(let i = 0; i < totalMessages.length(); i++){
+    if(totalMessages[i].reAwakable === "awakable"){
+      counterAwakable++;
+    } else if(totalMessages[i].reAwakable === "final"){
+      counterFinal++;
+    }
+  }
+  assert.strictEqual(numAwakable, counterAwakable);
+  assert.strictEqual(numFinal, counterFinal);
 });

f738eaf3e50d8e3f1458a9b5fda0df55ea45be17
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-07-07 10:51:35 +0200

    [PAGOPA-1075] feat: add enhancement for /info API

--- src/test/java/it/gov/pagopa/negativebizeventsdatastore/InfoTest.java
+++ src/test/java/it/gov/pagopa/negativebizeventsdatastore/InfoTest.java
@@ -1,7 +1,8 @@
 package it.gov.pagopa.negativebizeventsdatastore;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
@@ -10,6 +11,10 @@ import com.microsoft.azure.functions.HttpRequestMessage;
 import com.microsoft.azure.functions.HttpResponseMessage;
 import com.microsoft.azure.functions.HttpStatus;
 import java.util.Optional;
+import java.util.logging.Logger;
+
+import it.gov.pagopa.negativebizeventsdatastore.entity.AppInfo;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -30,16 +35,51 @@ class InfoTest {
     @SuppressWarnings("unchecked")
     HttpRequestMessage<Optional<String>> request = mock(HttpRequestMessage.class);
 
-    doReturn(builder).when(request).createResponseBuilder(any(HttpStatus.class));
-
     HttpResponseMessage responseMock = mock(HttpResponseMessage.class);
     doReturn(HttpStatus.OK).when(responseMock).getStatus();
+    doReturn(builder).when(builder).body(any());
     doReturn(responseMock).when(builder).build();
+    doReturn(builder).when(request).createResponseBuilder(any(HttpStatus.class));
+    doReturn(builder).when(builder).header(anyString(), anyString());
 
     // test execution
     HttpResponseMessage response = infoFunction.run(request, context);
 
     // test assertion
     assertEquals(HttpStatus.OK, response.getStatus());
+  }
+
+  @SneakyThrows
+  @Test
+  void getInfoOk() {
+
+    // Mocking service creation
+    Logger logger = Logger.getLogger("example-test-logger");
+    String path = "/META-INF/maven/it.gov.pagopa.negativebizeventsdatastore/negative-biz-events-datastore-function/pom.properties";
+
+    // Execute function
+    AppInfo response = infoFunction.getInfo(logger, path);
+
+    // Checking assertions
+    assertNotNull(response.getName());
+    assertNotNull(response.getVersion());
+    assertNotNull(response.getEnvironment());
+  }
+
+  @SneakyThrows
+  @Test
+  void getInfoKo() {
+
+    // Mocking service creation
+    Logger logger = Logger.getLogger("example-test-logger");
+    String path = "/META-INF/maven/it.gov.pagopa.negativebizeventsdatastore/negative-biz-events-datastore-function/fake";
+
+    // Execute function
+    AppInfo response = infoFunction.getInfo(logger, path);
+
+    // Checking assertions
+    assertNull(response.getName());
+    assertNull(response.getVersion());
+    assertNotNull(response.getEnvironment());
   }
 }

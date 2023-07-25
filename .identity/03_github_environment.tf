a0aaac7d5ba0695ea0e0c369a52c7d670d71f663
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-07-25 10:13:30 +0200

    [PAGOPA-1007] fix: updated .identity folder

--- .identity/03_github_environment.tf
+++ .identity/03_github_environment.tf
@@ -3,16 +3,16 @@ resource "github_repository_environment" "github_repository_environment" {
   repository  = local.github.repository
   # filter teams reviewers from github_organization_teams
   # if reviewers_teams is null no reviewers will be configured for environment
-  #  dynamic "reviewers" {
-  #    for_each = (var.github_repository_environment.reviewers_teams == null ? [] : [1])
-  #    content {
-  #      teams = matchkeys(
-  #        data.github_organization_teams.all.teams.*.id,
-  #        data.github_organization_teams.all.teams.*.name,
-  #        var.github_repository_environment.reviewers_teams
-  #      )
-  #    }
-  #  }
+  dynamic "reviewers" {
+    for_each = (var.github_repository_environment.reviewers_teams == null || var.env_short != "p" ? [] : [1])
+    content {
+      teams = matchkeys(
+        data.github_organization_teams.all.teams.*.id,
+        data.github_organization_teams.all.teams.*.name,
+        var.github_repository_environment.reviewers_teams
+      )
+    }
+  }
   deployment_branch_policy {
     protected_branches     = var.github_repository_environment.protected_branches
     custom_branch_policies = var.github_repository_environment.custom_branch_policies
@@ -64,7 +64,7 @@ resource "github_actions_environment_secret" "cluster_resource_group_name" {
   repository      = local.github.repository
   environment     = var.env
   secret_name     = "CLUSTER_RESOURCE_GROUP_NAME"
-  plaintext_value = local.aks_resource_group_name
+  plaintext_value = local.aks_cluster.resource_group_name
 }
 
 #tfsec:ignore:github-actions-no-plain-text-action-secrets # not real secret
@@ -72,7 +72,7 @@ resource "github_actions_environment_secret" "cluster_name" {
   repository      = local.github.repository
   environment     = var.env
   secret_name     = "CLUSTER_NAME"
-  plaintext_value = local.aks_name
+  plaintext_value = local.aks_cluster.name
 }
 
 #tfsec:ignore:github-actions-no-plain-text-action-secrets 
